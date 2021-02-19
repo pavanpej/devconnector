c9c65a4d48b7162b81daa8e119af95392914724f
Pavan Rao <pavanpej@gmail.com>
2021-02-19 09:32:01 +0530

    sync remote with local

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 const keys = {
   mongoURI:
-    "mongodb+srv://pavanpej:pavanpej@devconnector-pf2lo.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://pavanpej:pavanpej@devconnector-pf2lo.mongodb.net/test?retryWrites=true",
+  secretOrKey: "whatever"
 };
 
 module.exports = keys;
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -16,7 +22,11 @@ mongoose
   .then(() => console.log("MongoDB Connection Success"))
   .catch(err => console.log(`MongoDB Connection Failure - ${err}`));
 
-app.get("/", (req, res) => res.send("Hello World"));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
 
 // Use Routes
 app.use("/api/users", users);
